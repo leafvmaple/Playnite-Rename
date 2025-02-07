9b26e786f10d6d564c7a4f7bd10f3858d98aa89c
Zohar Lee <leafvmaple@gmail.com>
2025-02-07 18:19:49 +0800

    feat: start projects

---
+++ Rename.cs
@@ -0,0 +1,137 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using System.IO;
+
+namespace Rename
+{
+    public class Rename : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+        // private readonly IResourceProvider resources;
+
+        private RenameSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("89056061-1b70-40a1-8a17-07f01b24ea29");
+
+        public Rename(IPlayniteAPI api) : base(api)
+        {
+            settings = new RenameSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new RenameSettingsView();
+        }
+
+        public override IEnumerable<GameMenuItem> GetGameMenuItems(GetGameMenuItemsArgs args)
+        {
+            yield return new GameMenuItem
+            {
+                Description = "Name of game menu item",
+                Action = (_) =>
+                {
+                    // use args.Games to get list of games attached to the menu source
+                    Console.WriteLine("Invoked from game menu item!");
+                }
+            };
+        }
+
+        // To add new main menu items override GetMainMenuItems
+        public override IEnumerable<MainMenuItem> GetMainMenuItems(GetMainMenuItemsArgs args)
+        {
+            yield return new MainMenuItem
+            {
+                // MenuSection = "@Rename",
+                Description = "Rename File Name",
+                Action = (_) => OnMenuAction()
+            };
+        }
+
+        private void OnMenuAction()
+        {
+            var selectedGames = PlayniteApi.MainView.SelectedGames;
+            if (selectedGames == null || !selectedGames.Any())
+            {
+                PlayniteApi.Dialogs.ShowMessage("Select A Game");
+                return;
+            }
+
+            //var filePath = PlayniteApi.Dialogs.SelectFile("Select A File");
+            //if (string.IsNullOrEmpty(filePath))
+            //{
+            //    return;
+            //}
+
+            foreach (var game in selectedGames)
+            {
+                //try
+                //{
+                //    RenameFilesHelper.RenameFile(filePath, game.Name);
+                //}
+                //catch (Exception ex)
+                //{
+                //    logger.Error(ex, "Rename Failed!");
+                //    PlayniteApi.Dialogs.ShowErrorMessage($"Rename Failed：{ex.Message}", "error");
+                //}
+            }
+        }
+    }
+}
---
+++ RenameFilesHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rename
+{
+    using Playnite.SDK.Models;
+    using Playnite.SDK;
+    using System;
+    using System.IO;
+
+    public static class RenameFilesHelper
+    {
+        public static void RenameFile(string filePath, string newFileName)
+        {
+            if (string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(newFileName))
+            {
+                throw new ArgumentException("Path Is Empty!");
+            }
+
+            var newFilePath = Path.Combine(Path.GetDirectoryName(filePath), newFileName);
+            File.Move(filePath, newFilePath);
+        }
+    }
+}
